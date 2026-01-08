749c4f9f4ea09d87b1d0894d8489f03e1c285cdc
Corwin Riordan <Corwinriordan@gmail.com>
2026-01-08 17:32:06 -0500

    Week 1 Lecture Upload
    Projects from week 1, completed RockPaperScissors Clone & Started Guess A Number assignment

---
+++ GuessANumber/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+int value = Random.Shared.Next(1, 101);
+int maxGuesses = 10;
+int guessesRemaining = maxGuesses;
+while (true)
+{
+    Console.WriteLine("Guess a Number (1-100) in 10 tries: ");
+    bool valid = int.TryParse((Console.ReadLine() ?? "").Trim(), out int input);
+    
+    if (!valid) Console.WriteLine("Invalid.");
+    else if (input == value) break;
+    else if (input != value && guessesRemaing{
+        Console.WriteLine($"Incorrect. Too {(input < value ? "Low" : "High")}.");
+        guessesRemaining--;
+    }
+}
+
+Console.WriteLine("You Guessed It!.");
+Console.Write("Press any key to exit...");
+Console.ReadKey(true);
---
+++ RockPaperScissors/RockPaperScissors/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using static Move;
+
+int wins = 0;
+int draws = 0;
+int losses = 0;
+
+while (true)
+{
+    Console.Clear();
+    Console.WriteLine("Water, Grass, Fire, Air");
+    Console.WriteLine();
+
+    GetInput:
+        Console.Write("Choose [W]ater, [G]rass, or [F]ire, or [A]ir, or [E]xit:");
+        Move playerMove;
+        switch ((Console.ReadLine() ?? "").Trim().ToLower())
+    {
+        case "w" or "water": playerMove = Water; break;
+        case "g" or "grass": playerMove = Grass; break;
+        case "f" or "fire": playerMove = Fire; break;
+        case "a" or "air": playerMove = Air; break;
+        case "e" or "exit": Console.Clear(); return;
+        default: Console.WriteLine("Invalid Input. Try Again..."); goto GetInput;
+    }
+    
+    Move computerMove = (Move)Random.Shared.Next(4);
+    Console.WriteLine($"The Computer Chose {computerMove}.");
+    
+    switch (playerMove, computerMove)
+    {
+        case (Water, Grass) or (Grass, Fire) or (Fire, Water) or (Fire, Air):
+            Console.WriteLine("You Lose.");
+            losses++;
+            break;
+        case (Water, Fire) or (Grass, Water) or (Fire, Grass) or (Air, Fire):
+            Console.WriteLine("You Win.");
+            wins++;
+            break;
+        default:
+            Console.WriteLine("This game was a draw.");
+            draws++;
+            break;
+    }
+    
+    Console.WriteLine($"Score: {wins} wins, {losses} losses, {draws} draws");
+    Console.WriteLine("Press Enter To Continue...");
+    Console.ReadLine();
+}
+
+enum Move
+{
+    Water = 0,
+    Grass = 1,
+    Fire = 2,
+    Air = 3,
+}
+
