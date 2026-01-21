c285d42c2122eb029248658dca025c4940614785
Corwin Riordan <Corwinriordan@gmail.com>
2026-01-21 18:51:07 -0500

    Finished TicTacToe Modifications
    Completed Parts A,B,C, & D of the assignments, introduced new game modes such as PvP and CPU v CPU as well as a special rule that designates a random space to give that player a bonus turn. Also now includes numpad support for quick space selection.

--- TicTacToe/TicTacToe/Program.cs
+++ TicTacToe/TicTacToe/Program.cs
@@ -1,17 +1,88 @@
 ﻿using System;
 using System.Runtime.InteropServices;
 using System.Collections.Generic;
+using System.Threading;
 
 bool closeRequested = false;
-bool playerTurn = true;
+
+//Variables for each possible player
+bool playerTurnA = false;
+bool playerTurnB = false;
+bool compTurnA = false;
+bool compTurnB = false;
+
+//Variable for the gamemode type
+int gameMode = 0;
+
+//Tracking for coordinates of the special space
+int specialSpaceX;
+int specialSpaceY;
+
+//Triggers for whether the player receives a bonus turn or not
+bool specialTrigger = false;
+bool turnSkip = false;
+
 char[,] board;
 
-//One time random check to see if CPU should go first (not within while loop to only go once)
-int firstTurn = Random.Shared.Next(0, 2);
+
+// //One time random check to see if CPU should go first (not within while loop to only go once)
+// int firstTurn = Random.Shared.Next(0, 2);
+//     if (firstTurn == 1)
+//     {
+//         playerTurnA = false;
+//         compTurnB = true;
+//     }
+
+if (!closeRequested)
+    {
+        Console.WriteLine();
+        Console.WriteLine("""
+                Please Select Gamemode:
+                [1] - Player vs Computer
+                [2] - Player vs Player
+                [3] - Computer vs Computer
+                """);
+        GetInput:
+            //Console.CursorVisible = false;
+            switch (Console.ReadKey(true).Key)
+            {
+                case ConsoleKey.D1:     gameMode = 0; break;
+                case ConsoleKey.D2:     gameMode = 1; break;
+                case ConsoleKey.D3:     gameMode = 2; break;
+                default: goto GetInput; 
+            }    
+    }
+
+//One time randomizer of turns before going based on gamemode selection
+int firstTurn = Random.Shared.Next(0,2);
+if(gameMode == 0)
+{
+    if(firstTurn == 1)
+    {
+        compTurnA = true;
+    } else
+    {
+        playerTurnA = true;
+    }
+} else if (gameMode == 1)
+{
+    if(firstTurn == 1)
+    {
+        playerTurnB = true;
+    } else
+    {
+    playerTurnA = true;
+    }
+} else
+{
     if (firstTurn == 1)
     {
-        playerTurn = false;
+        compTurnA = true;
+    } else
+    {
+        compTurnB = true;
     }
+}
 
 while (!closeRequested)
 {
@@ -22,40 +93,117 @@ while (!closeRequested)
         {' ', ' ', ' ', },
     };
 
-    
-    
+    //Randomly chooses a "Special Space" that allows for the player to go twice in a row
+    specialTrigger = false;
+    specialSpaceX = Random.Shared.Next(0, 3);
+    specialSpaceY = Random.Shared.Next(0, 3);
 
     while (!closeRequested)
     {
-        if (playerTurn)
+        if (playerTurnA)
         {
-            PlayerTurn();
+            PlayerTurn('X', turnSkip);
+            turnSkip = false; 
             if (CheckForThree('X'))
             {
-                EndGame("  You Win.");
+                EndGame("  Player 1 Wins.");
                 //On win set playerTurn to false for start of next game (CPU goes first)
-                playerTurn = false;
+                playerTurnA = false;
+                if(gameMode == 1)
+                {
+                    playerTurnB = true;
+                } else
+                {
+                    compTurnA = true;
+                }
                 break;
-            }            
+            }   
+            //If the board had their marker in the special space trigger a turnskip of the opponent 
+            if (board[specialSpaceX, specialSpaceY] == 'X' && !specialTrigger)
+            {
+                turnSkip = true;
+                
+            }
+                   
         }
-        else
+        else if (playerTurnB)
         {
-            ComputerTurn();
+            PlayerTurn('O', turnSkip);
+            turnSkip = false;
             if (CheckForThree('O'))
             {
-                EndGame("  You Lose.");
-                //On loss set playerTurn to true for start of next game (Player goes first)
-                playerTurn = true;
+                EndGame("  Player 2 Wins.");
+                break;
+            }
+            if(board[specialSpaceX, specialSpaceY] == 'O' && !specialTrigger)
+            {
+                turnSkip = true;
+            }
+            
+        }
+        else if (compTurnA)
+        {
+            ComputerTurn('O');
+            if (CheckForThree('O'))
+            {
+                if(gameMode == 0)
+                {
+                    EndGame("  You Lose.");
+                    playerTurnA = true;
+                    break;
+                } else
+                {
+                    EndGame("  Computer O Wins.");
+                    compTurnB = true;
+                }
+                compTurnA = false;
                 break;
             }
         }
-        playerTurn = !playerTurn;
+        else if (compTurnB)
+        {
+            //Delay so each turn isn't taken immediately
+            Thread.Sleep(1000);
+            ComputerTurn('X');
+            Thread.Sleep(1000);
+            if (CheckForThree('X'))
+            {
+                EndGame(" Computer X Wins.");
+                compTurnB = false;
+                compTurnA = true;
+                break;
+            }
+            
+        }
+        //Logic to handle the switching of turns & skips this logic if the player has triggered a turn skip
+        if(gameMode == 0 && !turnSkip)
+        {
+            playerTurnA = !playerTurnA;
+            compTurnA = !compTurnA;
+            turnSkip = false;
+        } else if (gameMode == 1 && !turnSkip)
+        {
+            playerTurnA = !playerTurnA;
+            playerTurnB = !playerTurnB;
+            turnSkip = false;
+        } else if (gameMode == 2 && !turnSkip)
+        {
+            compTurnA = !compTurnA;
+            compTurnB = !compTurnB;
+            turnSkip = false;
+        } else
+        {
+            specialTrigger = true;
+        }
+        
+        //Finally check if the board is full for a draw.
         if (CheckForFullBoard())
         {
             EndGame("  Draw.");
             break;
         }
     }
+    
     if (!closeRequested)
     {
         Console.WriteLine();
@@ -73,7 +221,7 @@ while (!closeRequested)
     }
     Console.CursorVisible = true;
 
-    void PlayerTurn()
+    void PlayerTurn(char token, bool specialTrigger)
     {
         var (row, column) = (0, 0);
         bool moved = false;
@@ -82,7 +230,17 @@ while (!closeRequested)
             Console.Clear();
             RenderBoard();
             Console.WriteLine();
-            Console.WriteLine(" Use the arrow and enter keys to select a move.");
+            if(token == 'X' && !specialTrigger)
+            {
+                Console.WriteLine("It is Player 1's Turn.");
+            } else if (token == 'O' && !specialTrigger)
+            {
+                Console.WriteLine("It is Player 2's Turn.");
+            } else
+            {
+                Console.WriteLine("You Triggered Special Space Go Again!");
+            }
+            Console.WriteLine(" Use the arrow and enter keys to select a move or select the space using the NumPad [1-9].");
             Console.SetCursorPosition(column * 4 + 4, row * 2 + 4);
             Console.CursorVisible = true;
             switch (Console.ReadKey(true).Key)
@@ -94,7 +252,7 @@ while (!closeRequested)
                 case ConsoleKey.Enter:
                     if (board[row, column] is ' ')
                     {
-                        board[row, column] = 'X';
+                        board[row, column] = token;
                         moved = true;
                     }
                     break;
@@ -103,68 +261,69 @@ while (!closeRequested)
                     closeRequested = true;
                     break;
 
-
-                //Cases for numpad support
-                case ConsoleKey.NumPad1:
+                //Cases for numpad support (This should work however my keyboard is returning top row number codes when using the numpad)
+                //I don't know why but every keyboard i tested with returns Numpad keys like they are the normal top row numbers
+                //Therefore instead of ConsoleKey.NumPad* it is using ConsoleKey.D* for usability
+                case ConsoleKey.D1:
                     if (board[2, 0] is ' ')
                     {
-                        board[2, 0] = 'X';
+                        board[2, 0] = token;
                         moved = true;
                     }
                     break;
-                case ConsoleKey.NumPad2:
+                case ConsoleKey.D2:
                     if (board[2, 1] is ' ')
                     {
-                        board[2, 1] = 'X';
+                        board[2, 1] = token;
                         moved = true;
                     }
                     break;
-                case ConsoleKey.NumPad3:
+                case ConsoleKey.D3:
                     if (board[2, 2] is ' ')
                     {
-                        board[2, 2] = 'X';
+                        board[2, 2] = token;
                         moved = true;
                     }
                     break;
-                case ConsoleKey.NumPad4:
+                case ConsoleKey.D4:
                     if (board[1, 0] is ' ')
                     {
-                        board[1, 0] = 'X';
+                        board[1, 0] = token;
                         moved = true;
                     }
                     break;
-                case ConsoleKey.NumPad5:
+                case ConsoleKey.D5:
                     if (board[1 ,1] is ' ')
                     {
-                        board[1 ,1] = 'X';
+                        board[1 ,1] = token;
                         moved = true;
                     }
                     break;
-                case ConsoleKey.NumPad6:
+                case ConsoleKey.D6:
                     if (board[1, 2] is ' ')
                     {
-                        board[1, 2] = 'X';
+                        board[1, 2] = token;
                         moved = true;
                     }
                     break;
-                case ConsoleKey.NumPad7:
+                case ConsoleKey.D7:
                     if (board[0, 0] is ' ')
                     {
-                        board[0, 0] = 'X';
+                        board[0, 0] = token;
                         moved = true;
                     }
                     break;
-                case ConsoleKey.NumPad8:
+                case ConsoleKey.D8:
                     if (board[0, 1] is ' ')
                     {
-                        board[0, 1] = 'X';
+                        board[0, 1] = token;
                         moved = true;
                     }
                     break;
-                case ConsoleKey.NumPad9:
+                case ConsoleKey.D9:
                     if (board[0, 2] is ' ')
                     {
-                        board[0, 2] = 'X';
+                        board[0, 2] = token;
                         moved = true;
                     }
                     break;
@@ -173,8 +332,12 @@ while (!closeRequested)
         }
     }
 
-    void ComputerTurn()
+    void ComputerTurn(char token)
     {
+        //In the event of CPU vs CPU this allows for the board to render before the ending
+        Console.Clear();
+        RenderBoard();
+        Console.WriteLine();
         var possibleMoves = new List<(int X, int Y)>();
         for (int i = 0; i < 3; i++)
         {
@@ -188,7 +351,7 @@ while (!closeRequested)
         }
         int index = Random.Shared.Next(0, possibleMoves.Count);
         var (X, Y) = possibleMoves[index];
-        board[X, Y] = 'O';
+        board[X, Y] = token;
     }
 
     bool CheckForThree(char c) =>
